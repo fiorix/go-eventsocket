3131351b6c1dcc95de42e41c108205a0203bd2ba
Radu Ioan Fericean <radu@fericean.ro>
2018-03-28 23:00:41 +0300

    changed header to map[string]interface{}
    got some values of type []string e.g.
    "variable_DP_MATCH": ["198735558144398", "58144398"] and got errors
    from unmarshal

--- eventsocket/eventsocket.go
+++ eventsocket/eventsocket.go
@@ -229,8 +229,8 @@ func (h *Connection) readOne() bool {
 		for k, v := range tmp {
 			resp.Header[capitalize(k)] = v
 		}
-		if v, _ := resp.Header["_body"]; v != "" {
-			resp.Body = v
+		if v, _ := resp.Header["_body"]; v != nil {
+			resp.Body = v.(string)
 			delete(resp.Header, "_body")
 		} else {
 			resp.Body = ""
@@ -450,7 +450,7 @@ func (h *Connection) ExecuteUUID(uuid, appName, appArg string) (*Event, error) {
 }
 
 // EventHeader represents events as a pair of key:value.
-type EventHeader map[string]string
+type EventHeader map[string]interface{}
 
 // Event represents a FreeSWITCH event.
 type Event struct {
@@ -468,13 +468,16 @@ func (r *Event) String() string {
 
 // Get returns an Event value, or "" if the key doesn't exist.
 func (r *Event) Get(key string) string {
-	return r.Header[key]
+	if s, ok := r.Header[key].([]string); ok {
+		return strings.Join(s, ", ")
+	}
+	return r.Header[key].(string)
 }
 
 // GetInt returns an Event value converted to int, or an error if conversion
 // is not possible.
 func (r *Event) GetInt(key string) (int, error) {
-	n, err := strconv.Atoi(r.Header[key])
+	n, err := strconv.Atoi(r.Header[key].(string))
 	if err != nil {
 		return 0, err
 	}
